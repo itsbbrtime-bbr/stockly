a9d956aabd489ede0fd5fd88d909a9aeb5636543
itsbbrtime-bbr <itsbbrtime@gmail.com>
2026-08-25 21:46:17 +0900

    Update stock_analysis.py

--- stock_analysis.py
+++ stock_analysis.py
@@ -189,9 +189,9 @@ def fmt_pct(x, decimals=2):
 
 
 def print_section(title):
-    print("\n" + "=" * 120)
+    print("\n" + "=" * 33)
     print(title)
-    print("=" * 120)
+    print("=" * 33)
 
 
 def save_or_close_chart(filename):
